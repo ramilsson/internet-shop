276a1356c6cf6ff4f5e285ab6fecc4ddd6212e1f
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-29 09:31:36 +0500

    fix combineReducers params

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -2,4 +2,7 @@ import { combineReducers } from 'redux';
 import { cartReducer } from 'cart/cartSlice';
 import { goodsReducer } from 'goods/goodsSlice';
 
-export const reducer = combineReducers(cartReducer, goodsReducer);
+export const reducer = combineReducers({
+  cart: cartReducer,
+  goods: goodsReducer,
+});
