f6fc90d637a4844a09cb9db9a1525fa8ab8a2a09
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-29 11:25:23 +0500

    create loading and error selectors

--- src/goods/goodsSlice/selectors.js
+++ src/goods/goodsSlice/selectors.js
@@ -1,3 +1,11 @@
 export function getGoods(state) {
   return state.goods.items;
 }
+
+export function isGoodsLoading(state) {
+  return state.goods.isLoading;
+}
+
+export function getGoodsError(state) {
+  return state.goods.error;
+}
